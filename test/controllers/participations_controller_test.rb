5c3bf739bb0f8c9c76a18f04d4b00f41031805a9
Marco Brancaleoni <90571371+Marcobranceddu@users.noreply.github.com>
2023-02-23 18:26:29 +0100

    Update participations_controller_test.rb

--- test/controllers/participations_controller_test.rb
+++ test/controllers/participations_controller_test.rb
@@ -1,11 +1,36 @@
 # frozen_string_literal: true
-
 require 'test_helper'
-
 class ParticipationsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
   include Warden::Test::Helpers
 
+  # test 'should get index' do
+  #   # user = FactoryBot.create(:user)
+  #   # login_as(user, scope: :user)
+
+  #   user = User.create(email: "user@example.com", password: 'password', is_organizer: true)
+  #   new_event = {
+  #     name: 'Event 1',
+  #     image: 'URL',
+  #     start_date: '2023-01-01',
+  #     end_date: '2022-10-02',
+  #     max_participants: 12,
+  #     street: 'Viale Cavour 15',
+  #     city: 'Ferrara',
+  #     state: 'Emilia-Romagna',
+  #     country: 'Italy',
+  #     organizer_id: user.id,
+  #     latitude: 12.211213,
+  #     longitude: 12.211213
+  #   }
+  #   event = Event.create(new_event)
+  #   puts event.errors.messages
+  #   puts '/events/'+event.id.to_s+'/participations'
+  #   get '/events/'+event.id.to_s+'/participations'
+  #   # logout(:user)
+  #   assert_response :success
+  #   assert_not_nil assigns(:events)
+  # end
 
   test 'should create new event' do
     user = FactoryBot.create(:user)
@@ -31,7 +56,6 @@ class ParticipationsControllerTest < ActionDispatch::IntegrationTest
     logout(:user)
     assert_equal after, before + 1
   end
-
   test 'should update participation data' do
     user = FactoryBot.create(:user)
     login_as(user, scope: :user)
@@ -57,7 +81,6 @@ class ParticipationsControllerTest < ActionDispatch::IntegrationTest
     logout(:user)
     assert_equal after, before + 1
   end
-
   test 'should delete participations' do
     user = FactoryBot.create(:user)
     login_as(user, scope: :user)
