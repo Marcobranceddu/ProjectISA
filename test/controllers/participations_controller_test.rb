0ac1b2dad13be4cdec088cc809ed64ff0b180462
tguare <118912368+tguare@users.noreply.github.com>
2023-02-23 18:36:49 +0100

    Update participations_controller_test.rb

--- test/controllers/participations_controller_test.rb
+++ test/controllers/participations_controller_test.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
+
 require 'test_helper'
+
 class ParticipationsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
   include Warden::Test::Helpers
