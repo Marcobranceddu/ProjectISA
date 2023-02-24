2147325aaa5061c861f6ec549f5c3fa9a2186283
Marco Brancaleoni <90571371+Marcobranceddu@users.noreply.github.com>
2023-02-24 15:02:25 +0100

    Update participations_controller_test.rb

--- test/controllers/participations_controller_test.rb
+++ test/controllers/participations_controller_test.rb
@@ -6,6 +6,34 @@ class ParticipationsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
   include Warden::Test::Helpers
   
+  # test 'should get index' do
+  #   # user = FactoryBot.create(:user)
+  #   # login_as(user, scope: :user)
+
+  #   user = User.create(email: "user@example.com", password: 'password', is_organizer: true)
+  #   new_event = {
+  #     name: 'Event 1',
+  #     image: 'URL',
+  #     start_date: '2023-01-01',
+  #     end_date: '2022-10-02',
+  #     max_participants: 12,
+  #     street: 'Gulberg III',
+  #     city: 'Lahore',
+  #     state: 'Punjab',
+  #     country: 'Pakistan',
+  #     organizer_id: user.id,
+  #     latitude: 12.211213,
+  #     longitude: 12.211213
+  #   }
+  #   event = Event.create(new_event)
+  #   puts event.errors.messages
+  #   puts '/events/'+event.id.to_s+'/participations'
+  #   get '/events/'+event.id.to_s+'/participations'
+  #   # logout(:user)
+  #   assert_response :success
+  #   assert_not_nil assigns(:events)
+  # end
+
   test 'should create new event' do
     user = FactoryBot.create(:user)
     login_as(user, scope: :user)
